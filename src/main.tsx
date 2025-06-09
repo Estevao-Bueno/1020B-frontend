bf8244b16144bc61d1dc89db2f0676d0c4d2d47b
Estevao-Bueno <estevao.bueno@estudante.ifms.edu.br>
2025-06-09 11:29:43 -0400

    Props e States com class

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
-import Header from './Header.tsx'
-import Body from './Body.tsx'
-import Footer from './Footer.tsx'
+// import './index.css'
+
+import Conteiner from './Container.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
-    <Body />
-    <Footer />
+    <Conteiner nome='Header' />
+    <Conteiner nome='Body'  />
   </StrictMode>,
 )
